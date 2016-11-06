84d8e650e5207a65287a06cc0892a22887fd79c4
ivorobioff <igor.vorobioff@gmail.com>
2016-11-06 18:30:02 +0200

    added some stuff

--- views/index.php
+++ views/index.php
@@ -1,5 +1,65 @@
 <?php $this->layout('layout', ['title' => 'Sparrow']); ?>
 
 <?php $this->start('page');?>
-<h2>Sparrow Framework</h2>
+
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">BBB Inc.</a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav"></ul>
+                <button type="submit" class="btn btn btn-default btn-sm navbar-btn navbar-right">Sign In</button>
+            </div>
+        </div>
+    </nav>
+    <br><br><br>
+    <div class="container">
+        <div class="row">
+            <h1>Sign Up</h1>
+            <hr>
+            <div class="col-lg-6 well bs-component">
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="email" class="col-lg-2 control-label">Email</label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" id="email">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="col-lg-2 control-label">Password</label>
+                        <div class="col-lg-10">
+                            <input type="password" class="form-control" id="password">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmPassword" class="col-lg-2 control-label"></label>
+                        <div class="col-lg-10">
+                            <input type="password" class="form-control" id="confirmPassword">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        <label for="firstName" class="col-lg-2 control-label">First Name</label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" id="firstName">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="firstName" class="col-lg-2 control-label">Last Name</label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" id="lastName">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 <?php $this->stop('page'); ?>
--- views/layout.php
+++ views/layout.php
@@ -1,6 +1,23 @@
-<html>
-    <title><?=$this->escape($title)?></title>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?=$this->escape($title)?></title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/app.css">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="js/html5shiv.min.js"></script>
+        <script src="js/respond.min.js"></script>
+        <![endif]-->
+    </head>
     <body>
         <?=$this->section('page');?>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/app.js"></script>
     </body>
 </html>
